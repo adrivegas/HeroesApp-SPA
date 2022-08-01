57b676ee8df932c492b23047d701a342ee3d5b6c
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-01 11:34:16 -0500

    Se instala y configura React Router

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+
+import { DcPage } from "../heroes/pages/DcPage";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { MarvelPage } from "../heroes/pages/MarvelPage";
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage />} />
+
+                <Route path="login" element={<LoginPage />} />
+
+                <Route path="/" element={<Navigate to="/marvel" />} />
+            </Routes>
+        </>
+    )
+}
