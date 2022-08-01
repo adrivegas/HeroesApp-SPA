bbdfa78162ee1a0e8a4b00f598ec71a7f4eb0942
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-01 14:59:58 -0500

    Se coloca la clase de la ruta activa y se crean los archivos de barril

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 
-import { DcPage } from "../heroes/pages/DcPage";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { DcPage, MarvelPage } from "../heroes";
+import { LoginPage } from '../auth';
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar />
+
             <Routes>
                 <Route path="marvel" element={<MarvelPage />} />
                 <Route path="dc" element={<DcPage />} />
