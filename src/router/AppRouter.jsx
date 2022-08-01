212bcebb170b6292c3b12f7182d9b9e782648cfa
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-01 15:22:48 -0500

    Se crea un segundo router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,15 @@
-import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { DcPage, MarvelPage } from "../heroes";
+import { HeroesRoutes } from "../heroes";
 import { LoginPage } from '../auth';
-import { Navbar } from "../ui";
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
-
             <Routes>
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DcPage />} />
-
                 <Route path="login" element={<LoginPage />} />
+                <Route path="/*" element={<HeroesRoutes />} />
 
-                <Route path="/" element={<Navigate to="/marvel" />} />
             </Routes>
         </>
     )
