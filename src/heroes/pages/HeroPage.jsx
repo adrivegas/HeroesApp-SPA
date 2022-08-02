42d152f4b55bca4ac278721ecd4028677a4ad35e
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-02 10:39:08 -0500

    Actualización de diseño HeroPage, uso de useNavigate

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
@@ -10,6 +10,12 @@ export const HeroPage = () => {
   const hero = getHeroById(id);
   // console.log(hero);
 
+  const navigate = useNavigate();
+  // Navega una página atrás
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
+
   //Si es undefined nos dará un error al tratar de leer propiedades de 
   //undefined, por eso la siguiente condición: sino existe héroe navega a marvel
   if (!hero) {
@@ -17,8 +23,35 @@ export const HeroPage = () => {
   }
 
   return (
-    <>
-      <h1>{hero.superhero}</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Alter ego:</b> {hero.alter_ego} </li>
+          <li className="list-group-item"> <b>Publisher:</b> {hero.publisher} </li>
+          <li className="list-group-item"> <b>First appearance:</b> {hero.first_appearance} </li>
+        </ul>
+
+        <h5 className="mt-3"> Characters </h5>
+        <p>{hero.characters}</p>
+
+        <button
+          className="btn btn-outline-primary"
+          onClick={onNavigateBack}
+        >
+          Regresar
+        </button>
+
+      </div>
+
+    </div>
   )
 }
