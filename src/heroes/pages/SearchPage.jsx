c5224a7d79a4edd48424201a842cb3c1bdf259b8
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-02 14:49:31 -0500

    Mostrar y ocultar mensajes

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -10,8 +10,11 @@ export const SearchPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const { q = '' } = queryString.parse( location.search );
+  const { q = '' } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
+
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
 
   const { searchText, onInputChange } = useForm({
     searchText: q,
@@ -19,9 +22,9 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if ( searchText.trim().length <= 1 ) return;
-    console.log({searchText});
-    navigate(`?q=${ searchText }`);
+    // if ( searchText.trim().length <= 1 ) return;
+    // console.log({searchText});
+    navigate(`?q=${searchText}`);
   }
 
   return (
@@ -34,15 +37,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form onSubmit={ onSearchSubmit }>
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
-              value={ searchText }
-              onChange={ onInputChange }
+              value={searchText}
+              onChange={onInputChange}
             />
 
             <button className="btn btn-outline-primary mt-1">
@@ -54,15 +57,26 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">
+          {/* {
+            (q === '')
+              ? <div className="alert alert-primary">Search a hero</div>
+              : (heroes.length === 0)
+              && <div className="alert alert-danger">No hero with <b>{q}</b></div>
+          }
+          otra forma de hacerlo sería: */}
+
+          <div className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}>
             Search a hero
           </div>
-          <div className="alert alert-danger">
+
+          <div className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}>
             No hero with <b>{q}</b>
           </div>
 
           {
-            heroes.map( hero => (
+            heroes.map(hero => (
               <HeroCard key={hero.id} {...hero} />
             ))
           }
