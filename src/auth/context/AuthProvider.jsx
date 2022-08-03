4a18a7e206b741d86d90182ee13f516cc24a796b
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-03 15:15:15 -0500

    Login de un usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -2,16 +2,34 @@ import { useReducer } from "react"
 import { AuthContext } from "./AuthContext"
 import { authReducer } from "./authReducer";
 
+import { types } from "../types/types";
+
 const initialState = {
   logged: false,
 }
 
 export const AuthProvider = ({ children }) => {
-  // authState, dispatch pue4do cambiar el nombre y no hay problema
-  const { authState, dispatch } = useReducer( authReducer, initialState );
+  // authState, dispatch puedo cambiar el nombre y no hay problema
+  const [ authState, dispatch ] = useReducer( authReducer, initialState );
+
+  const login = ( name = '') => {
+
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'ABC',
+        name: name
+      }
+    }
+
+    dispatch(action);
+  }
 
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{
+      ...authState,
+      login: login
+    }}>
         { children }
     </AuthContext.Provider>
   )
