87acdfdfd90a9e42792705f30d56a537ec541c6d
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-03 15:59:44 -0500

    Mantener información del usuario activo usando LocalStorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,23 +4,34 @@ import { authReducer } from "./authReducer";
 
 import { types } from "../types/types";
 
-const initialState = {
-  logged: false,
+// const initialState = {
+//   logged: false,
+// }
+// ya no es necesario initialState ya que la función init se encarga de enviar esas propiedades
+
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  return {
+    logged: !!user,
+    user: user,
+  }
 }
 
 export const AuthProvider = ({ children }) => {
   // authState, dispatch puedo cambiar el nombre y no hay problema
-  const [ authState, dispatch ] = useReducer( authReducer, initialState );
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init );
 
   const login = ( name = '') => {
 
+    const user = { id: 'ABC', name: name }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name
-      }
+      payload: user
     }
+
+    localStorage.setItem('user', JSON.stringify(user));
 
     dispatch(action);
   }
