9ed1edfae87d30b6319cc7f7e0b3b59e253ab828
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-04 11:46:48 -0500

    Pruebas eb PrivateRoute

---
+++ tests/router/PrivateRoute.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { AuthContext } from "../../src/auth";
+import { PrivateRoute } from '../../src/router/PrivateRoute';
+
+describe('Pruebas en PrivateRoute', () => {
+
+    test('debe mostrar el children está autenticado', () => {
+
+        Storage.prototype.setItem = jest.fn();
+
+        const contextValue = {
+            logged: true,
+            user: {
+                id: 'abg',
+                name: 'Private'
+            }
+        }
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/search?q=batman']}>
+                    <PrivateRoute>
+                        <h1>Ruta privada</h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getAllByText('Ruta privada')).toBeTruthy();
+        expect(localStorage.setItem).toHaveBeenCalledWith('lastPath', '/search?q=batman');
+
+    });
+
+
+
+
+});
