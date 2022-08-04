0b3e1307edfc258ad1ba4de2fabcdb0ed3913436
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-04 11:27:47 -0500

    Prueba PublicRoute ---> caso si está autenticado

--- tests/router/PublicRoute.test.jsx
+++ tests/router/PublicRoute.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { AuthContext } from "../../src/auth";
 import { PublicRoute } from "../../src/router/PublicRoute";
 
@@ -21,5 +22,36 @@ describe('Pruebas en PublicRoute', () => {
         expect(screen.getAllByText('Ruta pública')).toBeTruthy();
 
     });
+
+    test('debe nevegar si está autenticado', () => {
+
+        const contextValue = {
+            logged: true,
+            user: {
+                name: 'MoonLight',
+                id: '2022'
+            }
+        }
+
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/login']}>
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1>Ruta pública</h1>
+                            </PublicRoute>
+                        } />
+                        <Route path='marvel' element={<h1>Página de marvel</h1>} />
+                    </Routes>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+        // screen.debug();
+
+        expect(screen.getByText('Página de marvel')).toBeTruthy();
+
+    });
+
 
 });
